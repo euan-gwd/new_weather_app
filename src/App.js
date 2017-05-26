8fb11865e25a18ecc0c9b57bdf5d4716c1c51d24
Euan_G <euan@amwic.co.za>
2017-05-26 15:33:37 +0200

    transfer complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import classNames from 'classnames';
+import Header from './components/Header';
+import CurrentWeather from './components/CurrentWeather';
+import ForecastWeather from './components/ForecastWeather';
+import './components/actions.css';
+import './fonts/fonts.css';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentIsActive: true,
+      forecastIsActive: false
+    };
+  } //end constructor
+
+  handleChangeToCurrent = () => {
+    (this.state.currentIsActive !== true)
+      ? this.setState({ currentIsActive: true, forecastIsActive: false })
+      : this.setState({ forecastIsActive: true, currentIsActive: false });
+
+  } //end handleChangeToMetric
+
+  handleChangeToForecast = () => {
+    (this.state.forecastIsActive !== true)
+      ? this.setState({ forecastIsActive: true, currentIsActive: false })
+      : this.setState({ currentIsActive: true, forecastIsActive: false });
+  } //end handleChangeToImperial
+
   render() {
+    let btnCurrent = classNames('button', { active: this.state.currentIsActive });
+    let btnForecast = classNames('button', { active: this.state.forecastIsActive });
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="background-image"></div>
+        <Header header="Local Weather" />
+        <div className="action-btn-group">
+          <button className={btnCurrent} onClick={this.handleChangeToCurrent}>Now</button>
+          <button className={btnForecast} onClick={this.handleChangeToForecast}>Forecast</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {(this.state.currentIsActive)
+          ? <CurrentWeather />
+          : null}
+        {(this.state.forecastIsActive)
+          ? <ForecastWeather />
+          : null}
       </div>
     );
   }
